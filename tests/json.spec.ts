b377863ae60ccba3c5a6d0208ce59b580f45aa09
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 <lgsduran@yahoo.com.br>
2026-04-24 20:10:19 -0300

    updates

--- tests/json.spec.ts
+++ tests/json.spec.ts
@@ -1,17 +1,13 @@
-// specs/person.spec.ts
 import { test } from '../fixtures/dataFixture';
 
 test.describe("Learning Playwright", () => {
   test('generate tests per person', async ({ persons }) => {
     for (const person of persons) {
-      //test(`Running test for ${person.name}`, async () => {
-        console.log(`Age: ${person.name}`);
-        console.log(`Age: ${person.age}`);
-        for (const phone of person.phoneNumbers) {
-          console.log(`Phone Number: ${phone.type} - ${phone.number}`);
-        }
-        //expect(person.age).toBeGreaterThan(0); // example assertion
-      //});
+      console.log(`Name: ${person.name}`);
+      console.log(`Age: ${person.age}`);
+      for (const phone of person.phoneNumbers) {
+        console.log(`Phone: ${phone.type} - ${phone.number}`);
+      }
     }
   });
 });
