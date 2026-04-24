282e2c6727e37e657d90ce4c9e3d0255266f7d0f
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 <lgsduran@yahoo.com.br>
2026-04-24 20:02:12 -0300

    remove typo

--- tests/excel.spec.ts
+++ tests/excel.spec.ts
@@ -7,7 +7,7 @@ test.describe("Learning Playwright", () => {
         for (const line of workBook) {
             console.log(`Name: ${line.name}`);
             console.log(`Age: ${line.age}`);
-            console.log(`Phone type: ${line.type})`);
+            console.log(`Phone type: ${line.type}`);
             console.log(`Phone number: ${line.phoneNumber}`);
             console.log("-----------------------------");
         }
