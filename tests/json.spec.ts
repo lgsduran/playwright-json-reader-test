6b12d3b38c4b6baaba6119d1f8e62c113282f849
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 <lgsduran@yahoo.com.br>
2026-05-01 20:53:33 -0300

    update

--- tests/json.spec.ts
+++ tests/json.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '../fixtures/dataFixture';
 
 test.describe("Learning Playwright", () => {
-  test('Running test for excel', async ({ personJson }) => {
+  test('Running test for Json', async ({ personJson }) => {
     for (const person of personJson) {
       console.log(`Name: ${person.name}`);
       console.log(`Age: ${person.age}`);
