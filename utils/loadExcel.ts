cc2dea8e207de63c03474e9e0c564fde2b094cf5
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 <lgsduran@yahoo.com.br>
2026-05-01 20:25:48 -0300

    update

--- utils/loadExcel.ts
+++ utils/loadExcel.ts
@@ -3,8 +3,7 @@ import * as fs from 'fs';
 import { PersonExcel } from '../modules/index';
 
 export const loadExcel = (filename: string): PersonExcel[] => {
-  const fileBuffer = fs.readFileSync(filename);
-  const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
+  const workbook = XLSX.read(fs.readFileSync(filename), { type: 'buffer' });
   const sheetName = workbook.SheetNames[0];
   const worksheet = workbook.Sheets[sheetName!]; 
   return worksheet ? XLSX.utils.sheet_to_json<PersonExcel>(worksheet) : [];
